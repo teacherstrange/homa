ec76ef45809a558d1ad8f16b17460b1a9b1c7ec3
Sam Goddard <hello@samgoddard.co.uk>
2022-09-27 15:13:42 +0100

    careers integration and surrounding page

--- pages/careers.js
+++ pages/careers.js
@@ -17,6 +17,13 @@ import DayInfo from '@/components/day-info'
 import MousePosition from '@/components/mouse-position'
 import TextScrambler from '@/components/text-scrambler'
 
+import LocationIcon from '@/icons/location.svg'
+import ClockIcon from '@/icons/clock.svg'
+import GraphIcon from '@/icons/graph.svg'
+import HappyIcon from '@/icons/happy.svg'
+import BookIcon from '@/icons/book.svg'
+import CashIcon from '@/icons/cash.svg'
+
 // Sanity
 // import SanityPageService from '@/services/sanityPageService'
 
@@ -45,7 +52,7 @@ import TextScrambler from '@/components/text-scrambler'
 
 // const pageService = new SanityPageService(query)
 
-export default function Careers(initalData) {
+export default function Careers({initalData, careers}) {
   // Sanity Data
   // const { data: { about } } = pageService.getPreviewHook(initialData)()
   
@@ -63,7 +70,7 @@ export default function Careers(initalData) {
           className=""
         >
           <m.div variants={fade}>
-            <div className="w-full bg-gradient-to-b from-pink to-pink/30 pt-24 lg:pt-40 xl:pt-52 border-b border-black/50 mx-auto relative overflow-hidden">
+            <div className="w-full bg-gradient-to-b from-pink to-pink/30 pt-24 lg:pt-40 xl:pt-52 border-black/50 mx-auto relative overflow-hidden">
               <div className="absolute top-0 right-0 mt-24 lg:mt-28 xl:mt-32 px-6 xl:px-10 text-[11px] uppercase tracking-widest font-medium leading-none text-right hidden lg:block">
                 <DayInfo className="mb-1" />
                 <MousePosition />
@@ -103,7 +110,7 @@ export default function Careers(initalData) {
                   <div className="col-span-10 col-start-2 md:col-span-10 md:col-start-2 md:border-l md:border-r border-black/50 py-[5vw] md:px-10">
                     <div className="grid grid-cols-12 pb-[10vw]">
                       <div className="col-span-12 lg:col-span-3 mb-5 lg:mb-0">
-                        <p className="text-2xl uppercase font-bold">[Icon]</p>
+                        <HappyIcon className="w-[40%] max-w-[50px] lg:max-w-[100px] lg:mx-auto" />
                       </div>
                       <div className="col-span-12 lg:col-span-7">
                         <span className="block font-black text-[clamp(35px,_5vw,_86px)] leading-[0.95] mb-8 lg:mb-12 uppercase max-w-[550px]">Trust from day one</span>
@@ -116,7 +123,7 @@ export default function Careers(initalData) {
 
                     <div className="grid grid-cols-12 pb-[10vw]">
                       <div className="col-span-12 lg:col-span-3 mb-5 lg:mb-0">
-                        <p className="text-2xl uppercase font-bold">[Icon]</p>
+                        <BookIcon className="w-[40%] max-w-[50px] lg:max-w-[100px] lg:mx-auto" />
                       </div>
                       <div className="col-span-12 lg:col-span-7">
                         <span className="block font-black text-[clamp(35px,_5vw,_86px)] leading-[0.95] mb-8 lg:mb-12 uppercase max-w-[550px]">Continuous learning</span>
@@ -127,9 +134,9 @@ export default function Careers(initalData) {
                       </div>
                     </div>
 
-                    <div className="grid grid-cols-12 pb-[10vw]">
+                    <div className="grid grid-cols-12 md:pb-[10vw]">
                       <div className="col-span-12 lg:col-span-3 mb-5 lg:mb-0">
-                        <p className="text-2xl uppercase font-bold">[Icon]</p>
+                        <CashIcon className="w-[40%] max-w-[50px] lg:max-w-[100px] lg:mx-auto" />
                       </div>
                       <div className="col-span-12 lg:col-span-7">
                         <span className="block font-black text-[clamp(35px,_5vw,_86px)] leading-[0.95] mb-8 lg:mb-12 uppercase max-w-[550px]">Competitive Cash</span>
@@ -142,9 +149,181 @@ export default function Careers(initalData) {
                   </div>
                 </div>
               </div>
+
+              <div className="w-full border-b border-black/50 hidden md:block">
+                <div className="grid grid-cols-12 max-w-screen-3xl mx-auto">
+                  <div className="col-span-10 col-start-2 md:col-span-10 md:col-start-2 md:border-l md:border-r border-black/50 py-[8vw] md:px-10">
+                    <div className="grid grid-cols-10 items-center">
+                      <div className="col-span-9 md:col-span-5 mb-12 md:mb-0">
+                        <a href="https://apply.workable.com/homa-games/" target="_blank" rel="noopener noreferrer" class="roll-btn inline-block">
+                          <span class="roll-btn__front">Join Homa</span>
+                          <span class="roll-btn__back">Join Homa</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <FooterCta />          
+            <div className="bg-lime text-black">
+              <div className="grid grid-cols-12 py-24 lg:py-[10vw] px-6 xl:px-10 max-w-screen-3xl mx-auto">
+                
+                <div className="col-span-12 z-10">
+                  <h2 className="display-text w-11/12 lg:w-9/12 mb-12 lg:mb-24 xl:mb-32">Success is its own reward. But there are perks as well.</h2>
+
+                  <div className="w-full flex flex-wrap border border-black/50 mb-6 lg:mb-8">
+                    <div className="w-full lg:w-1/3 xl:w-1/3 border-b lg:border-b lg:border-r border-black/50 p-5 lg:p-6 xl:p-8 2xl:p-10">
+
+                      <div className="content content--small w-11/12 flex flex-wrap h-full">
+                        <div className="w-full mb-24 content">
+                          <p>If you’re interested in working from our newly-renovated Paris HQ with a rooftop garden and WeWork amenities, we’ve got a desk right here...</p>
+                        </div>
+
+                        <LocationIcon className="w-1/4 max-w-[120px] mt-auto" />
+                      </div>
+                    </div>
+
+                    <div className="w-full lg:w-1/3 xl:w-1/3 border-b lg:border-b lg:border-r border-black/50 p-5 lg:p-6 xl:p-8 2xl:p-10">
+
+                      <div className="content content--small w-11/12 flex flex-wrap h-full">
+                        <div className="w-full mb-24 content">
+                          <p>Don’t live in France or have a right to work here? No worries, we offer sponsorship and relocation packages.</p>
+                        </div>
+
+                        <LocationIcon className="w-1/4 max-w-[120px] mt-auto" />
+                      </div>
+                    </div>
+
+                    <div className="w-full lg:w-1/3 xl:w-1/3 border-b lg:border-b lg:border-r-0 border-black/50 p-5 lg:p-6 xl:p-8 2xl:p-10">
+
+                      <div className="content content--small w-11/12 flex flex-wrap h-full">
+                        <div className="w-full mb-24 content">
+                          <p>Don't speak French and don't want to learn it? Pas de problème, we work in English.</p>
+                        </div>
+
+                        <LocationIcon className="w-1/4 max-w-[120px] mt-auto" />
+                      </div>
+                    </div>
+
+                    <div className="w-full lg:w-1/3 xl:w-1/3 border-b lg:border-b-0 lg:border-r border-black/50 p-5 lg:p-6 xl:p-8 2xl:p-10">
+
+                      <div className="content content--small w-11/12 flex flex-wrap h-full">
+                        <div className="w-full mb-24 content">
+                          <p>If you’re interested in working from our newly-renovated Paris HQ with a rooftop garden and WeWork amenities, we’ve got a desk right here...</p>
+                        </div>
+
+                        <LocationIcon className="w-1/4 max-w-[120px] mt-auto" />
+                      </div>
+                    </div>
+
+                    <div className="w-full lg:w-1/3 xl:w-1/3 border-b lg:border-b-0 lg:border-r border-black/50 p-5 lg:p-6 xl:p-8 2xl:p-10">
+
+                      <div className="content content--small w-11/12 flex flex-wrap h-full">
+                        <div className="w-full mb-24 content">
+                          <p>Don’t live in France or have a right to work here? No worries, we offer sponsorship and relocation packages.</p>
+                        </div>
+
+                        <LocationIcon className="w-1/4 max-w-[120px] mt-auto" />
+                      </div>
+                    </div>
+
+                    <div className="w-full lg:w-1/3 xl:w-1/3 border-b-0 lg:border-b-0 lg:border-r-0 border-black/50 p-5 lg:p-6 xl:p-8 2xl:p-10">
+
+                      <div className="content content--small w-11/12 flex flex-wrap h-full">
+                        <div className="w-full mb-24 content">
+                          <p>Don't speak French and don't want to learn it? Pas de problème, we work in English.</p>
+                        </div>
+
+                        <LocationIcon className="w-1/4 max-w-[120px] mt-auto" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <a href="https://apply.workable.com/homa-games/" target="_blank" rel="noopener noreferrer" class="roll-btn inline-block">
+                    <span class="roll-btn__front">Join Homa</span>
+                    <span class="roll-btn__back">Join Homa</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="">
+              <div className="relative z-0 flex overflow-x-hidden">
+                <div className="animate-marquee whitespace-nowrap">
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/500/500" alt="PLACEHOLDER" />
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/600/600" alt="PLACEHOLDER" />
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/700/700" alt="PLACEHOLDER" />
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/800/800" alt="PLACEHOLDER" />
+                </div>
+
+                <div className="absolute top-0 animate-marquee2 whitespace-nowrap">
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/500/500" alt="PLACEHOLDER" />
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/600/600" alt="PLACEHOLDER" />
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/700/700" alt="PLACEHOLDER" />
+                  <img className="inline-block w-[60%] md:w-[40%] xl:w-[30%] aspect-square" src="https://place.dog/800/800" alt="PLACEHOLDER" />
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full bg-pink/20 border-b border-black/50 mx-auto relative overflow-hidden">
+              <div className="grid grid-cols-12 max-w-screen-3xl mx-auto">
+                <div className="col-span-10 col-start-2 md:col-span-10 md:col-start-2 md:border-l md:border-r border-black/50 py-[10vw] md:px-16 xl:px-24">
+                  <h2 className="display-text w-8/12 xl:w-[60%] mb-8 lg:mb-24">Want to join us?</h2>
+                  <span className="uppercase text-sm tracking-widest mb-5 lg:mb-8 block font-medium">Open Roles</span>
+                  
+                  <div className="mb-8 lg:mb-12">
+                    {careers.jobs.slice(0,8).map((e, i) => {
+                      return (
+                        <a key={i} href={e.shortlink} target="_blank" rel="noreferrer noopener" className={`flex flex-wrap border-l border-r border-b border-black/50 p-6 lg:p-10 hover:bg-pink focus:bg-pink ${(i == 0) && 'border-t'}`}>
+                          <div className="w-full lg:w-2/3 pr-8 lg:pr-16 xl:pr-24 mb-6 lg:mb-0">
+                            <span className="block font-black text-[clamp(25px,_3vw,_66px)] leading-[0.95] uppercase">{e.title}</span>
+                          </div>
+                          <div className="w-full lg:w-1/3">
+                            <div className="w-full">
+                              <span className="flex uppercase font-medium tracking-wider text-base leading-none lg:leading-none xl:leading-none 2xl:leading-non w-11/12 border border-b-0 border-black/50">
+                                <div className="px-4 border-r border-black/50 flex items-center justify-center">
+                                  <LocationIcon className="w-[25px] lg:w-[30px]"/>
+                                </div>
+                                <div className="px-3 py-4 lg:py-5">
+                                  {e.city}
+                                </div>
+                              </span> 
+                            </div>
+                            <div className="w-full">
+                              <span className="flex uppercase font-medium tracking-wider text-base leading-none lg:leading-none xl:leading-none 2xl:leading-non w-11/12 border border-b-0 border-black/50">
+                                <div className="px-4 border-r border-black/50 flex items-center justify-center">
+                                  <ClockIcon className="w-[25px] lg:w-[30px]"/>
+                                </div>
+                                <div className="px-3 py-4 lg:py-5">
+                                  {e.employment_type}
+                                </div>
+                              </span>
+                            </div>
+                            <div className="w-full">
+                              <span className="flex uppercase font-medium tracking-wider text-base leading-none lg:leading-none xl:leading-none 2xl:leading-non w-11/12 border border-black/50">
+                                <div className="px-4 border-r border-black/50 flex items-center justify-center">
+                                  <GraphIcon className="w-[25px] lg:w-[30px]"/>
+                                </div>
+                                <div className="px-3 py-4 lg:py-5">
+                                  {e.department}
+                                </div>
+                              </span>
+                            </div>
+                          </div>
+                        </a>
+                      )
+                    })}
+                  </div>
+
+                  <a href="https://apply.workable.com/homa-games/" target="_blank" rel="noopener noreferrer" class="roll-btn inline-block">
+                    <span class="roll-btn__front">All Career Opportunities</span>
+                    <span class="roll-btn__back">All Career Opportunities</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
             <Footer />
           </m.div>
         </m.div>
@@ -161,3 +340,16 @@ export default function Careers(initalData) {
 //     props: { ...cms }
 //   }
 // }
+
+
+export async function getStaticProps(context) {
+  // const cms = await pageService.fetchQuery(context)
+
+  const res = await fetch(`https://apply.workable.com/api/v1/widget/accounts/homa-games?details=true`);
+
+  const careers = await res.json()
+
+  return {
+    props: { careers }
+  }
+}
