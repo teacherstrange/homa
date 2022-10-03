1a7686985df1330218ad147607eec5a485be07ba
Sam Goddard <hello@samgoddard.co.uk>
2022-10-03 23:12:38 +0100

    social share and favicon

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,12 +1,20 @@
 export default {
-  defaultTitle: 'Homa Games | Leave your mark on the gaming industry',
+  defaultTitle: 'Homa | Leave your mark on the gaming industry',
   description: 'Leave your mark in the gaming industry. We are building a team of extraordinary individuals working together to build apps that entertain the world. We are experts in every step of the publishing process.',
-  titleTemplate: 'Homa Games | %s',
+  titleTemplate: 'Homa | %s',
   openGraph: {
     type: 'website',
     locale: 'en_GB',
     url: 'https://homa.vercel.app/',
-    site_name: 'Homa Games',
+    site_name: 'Homa',
+    images: [
+      {
+        url: '/images/social.jpg',
+        width: 1600,
+        height: 900,
+        alt: `Homa Logo`,
+      },
+    ],
   },
   twitter: {
     handle: '@samuelgoddard',
