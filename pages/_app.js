e6e7a5fdef834ec54b7e062478cf9a87f8f4dba9
Sam Goddard <hello@samgoddard.co.uk>
2022-10-03 10:40:11 +0100

    preview mode wireup

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,8 @@ export default function App({ Component, pageProps }) {
     <>
       <DefaultSeo {...SEO} />
 
+      { pageProps.preview && (<div className={'fixed bottom-0 left-0 w-auto px-3 py-2 bg-red-600 text-white justify-center flex z-[200] uppercase font-mono text-sm m-3'}>Preview Mode - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click To Exit</a></div>)}
+
       <AnimatePresence exitBeforeEnter initial={false}>
         <Component {...pageProps} key={router.asPath} />
       </AnimatePresence>
